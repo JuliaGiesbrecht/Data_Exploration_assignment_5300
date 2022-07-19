7438fe00148d25edf83734ad9dbe4f6309b76048
JuliaGiesbrecht <jgiesbrecht@seattleu.edu>
2022-07-19 12:47:34 -0700

    code

--- Data_exploration_assignment_code.R
+++ Data_exploration_assignment_code.R
@@ -14,19 +14,33 @@ library(dplyr)
 ## Reading in Google trends data ##
 
 file_names <- list.files('Data/Lab3_Rawdata', pattern='trends_up_to_', full.names = TRUE) 
-all_csvs <- map_df(file_names, read_csv)
-head(all_csvs)
+google_trends <- map_df(file_names, read_csv)
 
 
-all_csvs$Month <- str_sub(all_csvs$monthorweek,1,10)
-all_csvs$Month <- ymd(all_csvs$Month)
+google_trends$Month <- str_sub(google_trends$monthorweek,1,10)
+google_trends$Month <- ymd(google_trends$Month)
 
+google_trends <- google_trends %>% 
+  group_by(schname, keyword) %>% 
+  mutate(Index_standard = (index - mean(index)) / sd(index))
+
+
+  
 
 ## Reading in Score Card Data ##
 
-score_Card <- read_csv('Data/Lab3_Rawdata/Most+Recent+Cohorts+(Scorecard+Elements).csv')
+score_card <- read_csv('Data/Lab3_Rawdata/Most+Recent+Cohorts+(Scorecard+Elements).csv')
 id_name_link <- read_csv('Data/Lab3_Rawdata/id_name_link.csv')
 
+id_name_link <- id_name_link %>% 
+  group_by(schname) %>% 
+  mutate(n = n()) %>% 
+  filter(n == 1)
+  
+## Joining Data together ##
 
-
-
+processed_data <- google_trends %>% 
+    inner_join(id_name_link, by = "schname") 
+    
+processed_data <- processed_data %>% 
+  inner_join(score_card, by = c("unitid" = "UNITID"))
